29dd02d89ddcf5f2b6c3db8f13df1924dc1d3c43
alex <alexstepanyak@gmail.com>
2024-05-10 05:56:40 +0300

    routes refactoring

--- src/routes/AuthorRoutes.ts
+++ src/routes/AuthorRoutes.ts
@@ -3,7 +3,7 @@ import {AuthorController} from "../controllers/AuthorController";
 import {authenticationMiddleware} from "../middleware/AuthenticationMiddleware";
 
 export default async function authorRoutes() {
-    app.post("/api/author/create",{
+    app.post("/api/author/create", {
         preHandler: authenticationMiddleware,
         handler: AuthorController.instance.createAuthor,
     });
